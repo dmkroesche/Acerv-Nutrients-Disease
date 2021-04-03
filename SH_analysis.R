dd88d1d98a7a17ac502a1b519fc51ad74e85f241
Danielle Kroesche <dmk32@miami.edu>
2021-04-03 11:15:56 -0400

    moved some files, added some analysis.

--- SH_analysis.R
+++ SH_analysis.R
@@ -11,55 +11,84 @@ library(Rmisc)
 rm(list=ls())
 
 # read in data file(s)
-# "Cleaned up T1 Nutrients s.H .csv contains T1 data with outliers and duplicates removed
-NDSH = read.csv("Cleaned up T1 Nutrients S.H .csv")
-T1 = subset(NDSH, Timepoint=T1)
-T1
+NDSH = read.csv("qPCR samples_R.csv")
 
-# attach function used so I don't need to type T1$variable.name each time. Make sure to detach later
-attach(T1)
+#subset samples into before and after disease exposure
+b4disease = subset(NDSH, Sequence=="OnlyNutrients")
+b4disease
 
-# Basic box plot
-SHbox <- ggplot(T1, aes(x=Nutrients, y=A.Acerv)) + 
+afterdisease = subset(NDSH, Sequence=="Disease")
+
+# scatterplot of S/H after nutrients but before disease
+
+
+# Basic box plot for before and after disease
+SHboxb4 <- ggplot(b4disease, aes(x=Nutrients, y=A.Acerv)) + 
   geom_boxplot()
-SHbox
+SHboxb4
 
-
-# Outliers removed (Tag#,Treatment,S/H) = (A113,Amb,43.9728968),(A120, Amb, 19.83590988), (A166, Amb, 18.73991716), (A047, Amb, 9.91795494), (A127, NH4,8.418380218), (A106, NH4,7.650439718),(A181, Amb, 7.950483896), (A109, Amb, 7.639841305)
-
+SHboxafter <- ggplot(afterdisease, aes(x=Nutrients, y=A.Acerv)) + 
+  geom_boxplot()
+SHboxafter
 
 #Using tapply to find the average S/H for each Nutrients treatment
-tapply(A.Acerv,Nutrients,mean)
+tapply(b4disease$A.Acerv,b4disease$Nutrients,mean)
+tapply(afterdisease$A.Acerv, afterdisease$Nutrients,mean)
 
-# summary of data
-summary=summarySE(T1,measurevar = 'A.Acerv',groupvars = c('Nutrients'))
-summary
+# summary of data before disease exposure
+summaryb4=summarySE(b4disease,measurevar = 'A.Acerv',groupvars = c('Nutrients'))
+summaryb4
 
-# bar graph of Ambient vs NH4 treatments
-SHbar <- ggplot(summary, aes(x=Nutrients, y=A.Acerv))
-SHbar <- SHbar+geom_col(fill='lightgray')
-SHbar=SHbar+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)
-SHbar
+#summary of data after disease exposure
+summaryafter=summarySE(afterdisease,measurevar='A.Acerv',groupvars=c('Nutrients'))
+summaryafter
 
-# using tapply to determine if variance is the same
-tapply(A.Acerv, Nutrients, var)
+# bar graph of Ambient vs NH4 treatments before diease exposure
+SHbarb4 <- ggplot(summaryb4, aes(x=Nutrients, y=A.Acerv))
+SHbarb4 <- SHbarb4+geom_col(fill='lightgray')
+SHbarb4 =SHbarb4+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)
+SHbarb4
+
+# bar graph of Ambient vs NH4 treatments after diease exposure
+SHbarafter <- ggplot(summaryafter, aes(x=Nutrients, y=A.Acerv))
+SHbarafter <- SHbarafter+geom_col(fill='lightgray')
+SHbarafter =SHbarafter+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)
+SHbarafter
+
+# using tapply to determine if variance is the same before disease
+tapply(b4disease$A.Acerv, b4disease$Nutrients, var)
 
 # variances are approximately the same. We can use the Bartlett test to test if variances are significantly different 
-bartlett.test(A.Acerv ~ Nutrients)
-# Bartlett test p-value = 0.7929, variances are not significantly different. So we can use a parametric test
+bartlett.test(b4disease$A.Acerv ~ b4disease$Nutrients)
+# Bartlett test p-value = 0.1876, variances are not significantly different. So we can use a parametric test
 
 # general t-test to test if the difference in the means is significant
-t.test(A.Acerv ~ Nutrients, var.equal=TRUE)
-# p-value = 0.1689 is not below 0.05 :( no significant difference
+t.test(b4disease$A.Acerv ~ b4disease$Nutrients, var.equal=TRUE)
+# p-value = 0.7469 is not below 0.05, no significant difference
 
 # specific t-test to see if S/H for Ambient is significantly greater than NH4
 # create vectors for each treatment
-####### Error: object of type 'closure' is not subsettable
-Ambient=A.Acerv[data$Nutrients=='Ambient']
-#here we do the same for green
-NH4=A.Acerv[data$Nutrients=='NH4']
-#now we can do a one-sided t-test
-t.test(Ambient,NH4,alternative = 'greater',var.equal=TRUE)
-#report the results as A one-sided T-test found ... significantly more Ambient treatment than NH4
-# (13+/-4 v 5+/-2) T(18)=-4.57, P=0.0001
+b4Ambient=A.Acerv[b4disease$Nutrients=='Ambient']
+b4Ambient
+
+b4NH4=A.Acerv[b4disease$Nutrients=='NH4']
+b4NH4
+
+# specific t-test to see if S/H for Ambient is significantly greater than NH4 after diease exposure
+# create vectors for each treatment
+afterAmbient=A.Acerv[afterdisease$Nutrients=='Ambient']
+afterAmbient
+
+afterNH4=A.Acerv[afterdisease$Nutrients=='NH4']
+afterNH4
+
+#now we can do a one-sided t-test for before disease exposure
+t.test(b4Ambient,b4NH4,alternative = 'greater',var.equal=TRUE)
+#report the results as "A one-sided T-test found that the S/H ratio is not significantly higher for corals exposed to the Ambient treatment than the NH4 treatment.
+
+#same thing for after disease exposure. One sided t-test
+t.test(afterAmbient,afterNH4,alternative = 'greater',var.equal=TRUE)
+
+#ANOVA
+
 
